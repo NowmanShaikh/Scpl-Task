6e2c676c9139979c6580cf4646fec2cf293979f9
NowmanShaikh <nowmanshaikh993@gmail.com>
2024-11-24 13:21:41 +0530

    Add files via upload

---
+++ Employee-Management-System/src/main/java/com/employee/management/model/Employee.java
@@ -0,0 +1,27 @@
+package com.employee.management.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "email")
+    private String email;
+
+}
